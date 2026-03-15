a33cb1a2c6bc361d654be5e0f1ede59963e3ef84
Peter Cullen <peter.cullen68@pm.me>
2026-03-15 03:00:51 -0400

    Part 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import './App.css'
-
-const Person = ({ person }) => {
-  return (
-    <li>{person.name} {person.number}</li>
-  )
-}
-
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterStart, setFilterStart] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const filteredArray = persons.filter(x => x.name.trim()
-                                                  .toLocaleUpperCase()
-                                                  .startsWith(
-                                                  filterStart.trim()
-                                                             .toLocaleUpperCase()))
+    .toLocaleUpperCase()
+    .startsWith(
+      filterStart.trim()
+        .toLocaleUpperCase()))
 
 
   const addPerson = (event) => {
@@ -50,7 +47,6 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setFilterStart(event.target.value)
-    console.log(event.target.value)
   }
 
   const handleNameChange = (event) => {
