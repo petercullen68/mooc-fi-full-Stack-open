d54cd2434bbe67d30a5bbf23244a649b3a85726a
Peter Cullen <peter.cullen68@pm.me>
2026-03-20 13:08:47 -0400

    Parts 3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,120 +1,87 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const morgan = require("morgan")
+const mongoose = require("mongoose");
+const logger = require("./middleware/logger");
+const Person = require("./models/person");
 
+const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
+app.use(logger);
 
-morgan.token("bodyToJson", function (req, res) {
-  return JSON.stringify(req.body);
-});
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"),
-    "-",
-    tokens["response-time"](req, res),
-    "ms",
-    tokens["bodyToJson"](req, res),
-  ].join(" ");
-}));
-
-let phonebook = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
+// Converted
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const phonebookEntry = phonebook.find((person) => person.id === id);
-  if (!phonebookEntry) {
-    return response.status(404).end();
-  }
-  response.json(phonebookEntry);
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  }).catch((error) => {
+    response.status(400).end();
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const phonebookEntry = phonebook.find((person) => person.id === id);
-  if (!phonebookEntry) {
-    return response.status(404).end();
-  }
-  phonebook = phonebook.filter((phonebookEntry) => phonebookEntry.id !== id);
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      response.status(400).end();
+    });
 });
 
-const generateId = () => {
-  const min = 1; 
-  const max = 10000000; 
-  return String(Math.floor(Math.random() * (max - min + 1)) + min);
-};
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
-    let contentMissing = ""
+    let contentMissing = "";
     if (!body.name) {
-      contentMissing = "body"
+      contentMissing = "body";
     }
     if (!body.number) {
       contentMissing += " number";
     }
-    contentMissing = contentMissing.trim()
+    contentMissing = contentMissing.trim();
     return response.status(400).json({
       error: `content missing - [${contentMissing}]`,
     });
   }
 
-  const checkForExistence = (phonebook.findIndex(x => x.name == body.name) >= 0)
-  if (checkForExistence) {
-    return response.status(409).json({
-      error: "name already exists",
-    });
-  }
+  //const checkForExistence =
+  //  phonebook.findIndex((x) => x.name == body.name) >= 0;
+  //if (checkForExistence) {
+  //  return response.status(409).json({
+  //    error: "name already exists",
+  //  });
+  //}
 
-  const phonebookEntry = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
-    number: body.number,
-  };
-
-  phonebook = phonebook.concat(phonebookEntry);
-
-  response.json(phonebookEntry);
+    number: body.number
+  }) 
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 });
 
 app.get("/api/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${phonebook.length} people</p>\n<p>${new Date()}</p>`,
-  );
+  Person.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p>\n<p>${new Date()}</p>`,
+    );
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/phonebook/middleware/logger.js
@@ -0,0 +1,20 @@
+const morgan = require("morgan");
+
+morgan.token("bodyToJson", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+const logger = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["bodyToJson"](req, res),
+    ].join(" ");
+  });
+
+module.exports = logger
