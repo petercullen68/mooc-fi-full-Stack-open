fe1112b8276e65cf2a194a3560b39fe614578f79
Peter Cullen <peter.cullen68@pm.me>
2026-03-25 14:48:39 -0400

    Parts 9.6-9.7 (fixes)

--- part9/calculators/utils.ts
+++ part9/calculators/utils.ts
@@ -7,10 +7,25 @@ export const isFiniteNumberZeroOrPositive = (v: unknown): v is number =>
 export const isFinitePositiveArray = (value: unknown): value is number[] =>
   Array.isArray(value) && value.every(isFinitePositiveNumber);
 
-export const isFiniteZeroOrPositiveArray = (value: unknown): value is number[] =>
+export const isFiniteZeroOrPositiveArray = (
+  value: unknown,
+): value is number[] =>
   Array.isArray(value) && value.every(isFiniteNumberZeroOrPositive);
 
-export const hasProp = (obj: unknown, prop: string): obj is Record<string, unknown> =>
+export const hasProp = (
+  obj: unknown,
+  prop: string,
+): obj is Record<string, unknown> =>
   typeof obj === "object" &&
   obj !== null &&
   Object.prototype.hasOwnProperty.call(obj, prop);
+
+type Query = Record<string, unknown>;
+
+export const hasQueryParam = (query: Query, key: string): boolean =>
+  Object.prototype.hasOwnProperty.call(query, key);
+
+export const getStringQueryParam = (query: Query, key: string): string | null => {
+  const value = query[key];
+  return typeof value === "string" ? value : null;
+};
